139e017a8114c92aa234b88bbd299b4c1a27634f
Tobias Hendricks <tobias.hendricks@dermalog.com>
2019-01-25 21:53:37 +0100

    Added some Postprocessing and CameraControl

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float MoveSpeed = 20f;
+    public float XScale = 1f;
+    public float YScale = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + new Vector3(Input.GetAxis("Horizontal") * XScale, 0, Input.GetAxis("Vertical") * YScale) * MoveSpeed * Time.deltaTime;
+    }
+}
