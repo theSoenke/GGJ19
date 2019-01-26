6c3f58b4505910f4f42809142da64dade726cdf0
gurkenhobel <nilsw.agner@web.de>
2019-01-26 14:47:20 +0100

    player animation + camera stuff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float movementSpeed = 1.0f;
+    public float animationWalkSpeedFactor = 1.0f;
 
     private Rigidbody rigidbodyComponent;
+    private Animator animator;
     private Vector3 prevInputVector = new Vector3();
+
+    
+    private const float SMALL_FLOAT = 0.0001f;
 
     private void Start()
     {
         rigidbodyComponent = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
         prevInputVector = new Vector3(1.0f, 0.0f, 0.0f);
     }
 
@@ -20,7 +27,15 @@ public class PlayerController : MonoBehaviour
             Shoot();
         }
 
+        UpdateAnimator();
         UpdatePosition();
+        UpdateRotation();
+    }
+
+    private void UpdateAnimator()
+    {
+        var animationWalkSpeed = animationWalkSpeedFactor * rigidbodyComponent.velocity.magnitude;
+        animator.SetFloat("WalkSpeed", animationWalkSpeed);
     }
 
     private void Shoot()
@@ -28,19 +43,27 @@ public class PlayerController : MonoBehaviour
         Debug.Log("Shoot");
     }
 
-    void UpdatePosition()
+    private void UpdatePosition()
     {
         var inputVector = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
         if (inputVector.sqrMagnitude >= 1.0f)
         {
             inputVector = inputVector.normalized;
         }
-        if (inputVector.sqrMagnitude >= 0.0001)
+        if (inputVector.sqrMagnitude >= SMALL_FLOAT)
         {
             prevInputVector = inputVector.normalized;
         }
 
         var speedVector = inputVector * movementSpeed;
         rigidbodyComponent.velocity = speedVector;
+    }
+
+    private void UpdateRotation()
+    {
+        if(rigidbodyComponent.velocity.magnitude >= SMALL_FLOAT) {
+            var forwardsVector = rigidbodyComponent.velocity.normalized;
+            transform.rotation = Quaternion.LookRotation(forwardsVector);
+        }
     }
 }
